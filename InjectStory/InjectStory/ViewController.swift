2cadc733327478e8379b3ee0bf28611acf45d0dd
Maciej Gad <gad.maciej@gmail.com>
2016-10-30 17:01:22 +0100

    add empty project

---
+++ InjectStory/InjectStory/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InjectStory
+//
+//  Created by Maciej Gad on 30.10.2016.
+//  Copyright © 2016 Maciej Gad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
